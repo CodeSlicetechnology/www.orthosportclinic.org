b1729efe7d8ab0698dbc89bc0f0c929367f0ec68
deekshithd-at-225049767740 <deekshidike@gmail.com>
2021-06-26 15:33:35 +0530

    updated

--- resources/views/website/gallery.blade.php
+++ resources/views/website/gallery.blade.php
@@ -10,18 +10,24 @@
     <section class="blog-area ptb-100 top-first-section">
 		<div class="container">
 			<div class="row">
-				@foreach ($data['galleryImages'] as $item)
-                    <div class="col-lg-4 col-md-6 pb-3">
-                        <div class="single-blog mobile-width-60 m-auto">
-                            <img src="{{ asset('./'.$item->image_path) }}" alt="Image">
-                            @if ($item->description != null)
-                                <div class="blog-content p-3">
-                                    <p class="mb-0">{{ $item->description }}</p>
-                                </div>
-                            @endif                            
+                @if (count($data['galleryImages']) > 0)
+                    @foreach ($data['galleryImages'] as $item)
+                        <div class="col-lg-4 col-md-6 pb-3">
+                            <div class="single-blog mobile-width-60 m-auto">
+                                <img src="{{ asset('./'.$item->image_path) }}" alt="Image">
+                                @if ($item->description != null)
+                                    <div class="blog-content p-3">
+                                        <p class="mb-0">{{ $item->description }}</p>
+                                    </div>
+                                @endif                            
+                            </div>
                         </div>
+                    @endforeach
+                @else
+                    <div class="col-12 text-center ptb-100 mb-5">
+                        <p>No images found</p>
                     </div>
-                @endforeach
+                @endif				
 			</div>
 		</div>
 	</section>
