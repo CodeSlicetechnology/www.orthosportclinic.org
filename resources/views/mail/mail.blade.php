97a50bafc455c18a3ca6783d4f9206d2b0596125
deekshithd-at-225049767740 <deekshidike@gmail.com>
2021-07-07 23:48:24 +0530

    updated mail

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -1,5 +1,5 @@
 <h3>Contact Details</h3>
-<p style="font-size: 16px;">Name: {{ $name }}</p>
+<p style="font-size: 14px;">Name: {{ $name }}</p>
 <p style="font-size: 14px;">Email: {{ $email }}</p>
 <p style="font-size: 14px;">Phone: {{ $phone }}</p>
 <p style="font-size: 14px;">Subject: {{ $subject }}</p>
